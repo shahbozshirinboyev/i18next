de9e94acc1a99351d420a007b36fad49c99b8f45
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-02-05 10:15:48 +0500

    fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useTranslation } from 'react-i18next';
-import i18n  from './i18n.js';
+import { useTranslation } from "react-i18next";
+import i18n from "./i18n.js";
 
 function App() {
   const { t } = useTranslation();
@@ -11,36 +11,64 @@ function App() {
           <a className="btn btn-ghost text-xl">i18next</a>
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <details>
-                <summary><i className="bi bi-translate"></i></summary>
-                <ul className="bg-base-100 rounded-t-none p-2">
-                  <li>
-                    <a onClick={() => i18n.changeLanguage('en')} className="text-nowrap">English</a>
-                  </li>
-                  <li>
-                    <a onClick={() => i18n.changeLanguage('ru')} className="text-nowrap">Russian</a>
-                  </li>
-                  <li>
-                    <a onClick={() => i18n.changeLanguage('ko')} className="text-nowrap">Korean</a>
-                  </li>
-                  <li>
-                    <a onClick={() => i18n.changeLanguage('uz')} className="text-nowrap">O'zbek</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-          </ul>
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-sm">
+              <i className="bi bi-translate"></i>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+            >
+              <li>
+                <a onClick={() => i18n.changeLanguage("en")}>
+                  <img
+                    src="/img/language_icons/en.png"
+                    alt=""
+                    className="w-5"
+                  />
+                  <span>English</span>
+                </a>
+              </li>
+              <li>
+                <a onClick={() => i18n.changeLanguage("ru")}>
+                  <img
+                    src="/img/language_icons/ru.png"
+                    alt=""
+                    className="w-5"
+                  />
+                  <span>Russian</span>
+                </a>
+              </li>
+              <li>
+                <a onClick={() => i18n.changeLanguage("ko")}>
+                  <img
+                    src="/img/language_icons/ko.png"
+                    alt=""
+                    className="w-5"
+                  />
+                  <span>Korean</span>
+                </a>
+              </li>
+              <li>
+                <a onClick={() => i18n.changeLanguage("uz")}>
+                  <img
+                    src="/img/language_icons/uz.png"
+                    alt=""
+                    className="w-5"
+                  />
+                  <span>O'zbekcha</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       {/* Navbar - end */}
 
       <div className="border border-gray-400 rounded-lg flex flex-col justify-center items-center p-4">
-        <h1 className='block'>{t('header.title')}</h1>
-        <p>{t('header.subtitle')}</p>
+        <h1 className="block">{t("header.title")}</h1>
+        <p>{t("header.subtitle")}</p>
       </div>
-
     </div>
   );
 }
--- src/i18n.js
+++ src/i18n.js
@@ -1,20 +1,20 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next'; // React uchun
-import HttpBackend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next"; // React uchun
+import HttpBackend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(HttpBackend) // JSON fayllarni yuklash uchun
   .use(LanguageDetector) // Brauzer tilini aniqlash
   .use(initReactI18next) // React integratsiyasi
   .init({
-    fallbackLng: 'en', // Asosiy til
+    fallbackLng: "en", // Asosiy til
     debug: false, // Debug rejimi
     interpolation: {
       escapeValue: false, // XSS hujumlaridan himoya
     },
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json', // JSON fayllar manzili
+      loadPath: "/locales/{{lng}}/{{ns}}.json", // JSON fayllar manzili
     },
   });
 
