634000066ecce2eb1ee825d2766789b302b46f89
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-02-05 10:03:11 +0500

    i18next library connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { useTranslation } from 'react-i18next';
+import i18n  from './i18n.js';
 
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="container">
       {/* Navbar - start */}
       <div className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">i18next</a>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
@@ -15,16 +17,16 @@ function App() {
                 <summary><i className="bi bi-translate"></i></summary>
                 <ul className="bg-base-100 rounded-t-none p-2">
                   <li>
-                    <a className="text-nowrap">English</a>
+                    <a onClick={() => i18n.changeLanguage('en')} className="text-nowrap">English</a>
                   </li>
                   <li>
-                    <a className="text-nowrap">Russian</a>
+                    <a onClick={() => i18n.changeLanguage('ru')} className="text-nowrap">Russian</a>
                   </li>
                   <li>
-                    <a className="text-nowrap">Korean</a>
+                    <a onClick={() => i18n.changeLanguage('ko')} className="text-nowrap">Korean</a>
                   </li>
                   <li>
-                    <a className="text-nowrap">O'zbek</a>
+                    <a onClick={() => i18n.changeLanguage('uz')} className="text-nowrap">O'zbek</a>
                   </li>
                 </ul>
               </details>
@@ -34,10 +36,9 @@ function App() {
       </div>
       {/* Navbar - end */}
 
-      <div className="border flex justify-center items-center">
-        <p className="p-4">
-          Hello, World!
-        </p>
+      <div className="border border-gray-400 rounded-lg flex flex-col justify-center items-center p-4">
+        <h1 className='block'>{t('header.title')}</h1>
+        <p>{t('header.subtitle')}</p>
       </div>
 
     </div>
---
+++ src/i18n.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next'; // React uchun
+import HttpBackend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(HttpBackend) // JSON fayllarni yuklash uchun
+  .use(LanguageDetector) // Brauzer tilini aniqlash
+  .use(initReactI18next) // React integratsiyasi
+  .init({
+    fallbackLng: 'en', // Asosiy til
+    debug: false, // Debug rejimi
+    interpolation: {
+      escapeValue: false, // XSS hujumlaridan himoya
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json', // JSON fayllar manzili
+    },
+  });
+
+export default i18n;
